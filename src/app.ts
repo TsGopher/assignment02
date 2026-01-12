4481d772e30225f091f208cf8dd211b754624093
ibntofajjal <joynal.bintofajjal@gmail.com>
2026-01-12 16:06:18 +0600

    remove index route auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
 import express, { Request, Response } from "express";
 import { initDB } from "./config/db";
-import auth from "./middlewares/auth";
 import { authRoute } from "./modules/auth/auth.route";
-import { ROLE } from "./modules/auth/auth.schema";
-import { vehiclesRoute } from "./modules/vehicles/vehicles.route";
-import { userRoute } from "./modules/users/user.route";
 import { bookingRoute } from "./modules/bookings/booking.route";
+import { userRoute } from "./modules/users/user.route";
+import { vehiclesRoute } from "./modules/vehicles/vehicles.route";
 
 const app = express();
 
@@ -16,11 +14,11 @@ initDB();
 const api_versioning = "/api/v1";
 
 app.use(`${api_versioning}/auth`, authRoute);
-app.use(`${api_versioning}/vehicles`, vehiclesRoute)
-app.use(`${api_versioning}/users`, userRoute)
-app.use(`${api_versioning}/bookings`, bookingRoute)
+app.use(`${api_versioning}/vehicles`, vehiclesRoute);
+app.use(`${api_versioning}/users`, userRoute);
+app.use(`${api_versioning}/bookings`, bookingRoute);
 
-app.get("/", auth(ROLE.CUSTOMER), (req, res) => {
+app.get("/", (req, res) => {
   res.send("Vehicle Rent: Application 🎉");
 });
 
